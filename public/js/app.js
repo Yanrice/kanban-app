711396909c52dee525b9c6b822d96c1e10951111
Yannick  Kalukuta <yannickkalukuta@gmail.com>
2025-07-18 02:18:12 -0400

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,5 @@
-
+if (localStorage.getItem('token')) {
+    document.getElementById('auth-container').style.display = 'none';
+    document.getElementById('board-container').style.display = 'block';
+    boardManager.init();
+}
